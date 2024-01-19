ae15fc91cb4ec16205425a2320d0a56e0aec034f
Samuel Walters <144137864+Samuel-Walters-Student@users.noreply.github.com>
2024-01-19 15:07:55 +0000

    hi ari

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+
+Console.WriteLine("_͟1͟_͟|͟_͟X_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+
+
+    }
+    public static void xInTopLeft(){
+Console.WriteLine("_͟X_͟|͟_͟2͟_͟|͟_͟3͟_͟");
+Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+Console.WriteLine(" 7 | 8 | 9 ");
+}
+}
+
+    public static void xInTopMiddle(){}
