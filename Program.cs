8fccb9abb28a1bf13045db8e251139a3c877d425
Samuel Walters <144137864+Samuel-Walters-Student@users.noreply.github.com>
2024-01-19 15:30:13 +0000

    end of day 1

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("_͟1͟_͟|͟_͟2͟_͟|͟_͟3͟_͟");
-        Console.WriteLine("_͟4͟_͟|͟_͟5͟_͟|͟_͟6͟_͟");
+        Console.WriteLine(" ͟1͟ ͟|͟ ͟2͟ ͟|͟ ͟3͟ ");
+        Console.WriteLine(" ͟4͟ ͟|͟ ͟5͟ ͟|͟ ͟6͟ ");
         Console.WriteLine(" 7 | 8 | 9 ");
+        Console.ReadLine();
     }
     public static void xInTopLeft()
     {
